1be4e22f8ee19a0c14ce9e47a4922209925c36c5
grassEqualsBugs <eizprintz@gmail.com>
2025-05-15 14:51:48 -0700

    Level functionality

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,3 +1,7 @@
+import Level from "./Level.js";
+import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
+import { loadBackgroundSprites } from "./sprites.js";
+
 export async function loadImage(url) {
 	return new Promise((resolve) => {
 		const image = new Image();
@@ -8,6 +12,21 @@ export async function loadImage(url) {
 
 export async function loadLevel(name) {
 	const response = await fetch(`levels/${name}.json`);
-	const data = await response.json();
-	return data;
+
+	const [levelSpec, backgroundSprites] = await Promise.all([
+		response.json(),
+		loadBackgroundSprites(),
+	]);
+
+	const level = new Level();
+	const backgroundLayer = createBackgroundLayer(
+		levelSpec.backgrounds,
+		backgroundSprites,
+	);
+	level.comp.layers.push(backgroundLayer);
+
+	const spriteLayer = createSpriteLayer(level.entities);
+	level.comp.layers.push(spriteLayer);
+
+	return level;
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,33 @@
-import Compositor from "./Compositor.js";
 import Timer from "./Timer.js";
 import { loadLevel } from "./loaders.js";
-import { loadBackgroundSprites } from "./sprites.js";
 import { createMario } from "./entities.js";
-import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
-
 import Keyboard from "./KeyboardState.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-Promise.all([createMario(), loadBackgroundSprites(), loadLevel("1-1")]).then(
-	async ([mario, backgroundSprites, level]) => {
-		const comp = new Compositor();
-		const backgroundLayer = createBackgroundLayer(
-			level.backgrounds,
-			backgroundSprites,
-		);
-		comp.layers.push(backgroundLayer);
+Promise.all([createMario(), loadLevel("1-1")]).then(async ([mario, level]) => {
+	const gravity = 2000;
+	mario.pos.set(64, 80);
 
-		const gravity = 2000;
-		mario.pos.set(64, 180);
+	level.entities.add(mario);
 
-		const input = new Keyboard();
-		input.addMapping(" ", (keyState) => {
-			if (keyState) {
-				mario.jump.start();
-			} else {
-				mario.jump.cancel();
-			}
-		});
-		input.listenTo(window);
+	const input = new Keyboard();
+	input.addMapping(" ", (keyState) => {
+		if (keyState) {
+			mario.jump.start();
+		} else {
+			mario.jump.cancel();
+		}
+	});
+	input.listenTo(window);
 
-		const spriteLayer = createSpriteLayer(mario);
-		comp.layers.push(spriteLayer);
+	const timer = new Timer(1 / 60);
+	timer.update = function update(deltaTime) {
+		level.update(deltaTime);
+		level.comp.draw(context);
+		mario.vel.y += gravity * deltaTime;
+	};
 
-		const timer = new Timer(1 / 60);
-		timer.update = function update(deltaTime) {
-			mario.update(deltaTime);
-			comp.draw(context);
-			mario.vel.y += gravity * deltaTime;
-		};
-
-		timer.start();
-	},
-);
+	timer.start();
+});
