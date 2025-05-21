6d75490c8984c8658727959511e896d80540133b
grassEqualsBugs <eizprintz@gmail.com>
2025-05-21 08:34:50 -0700

    Small stylistic changes

--- public/js/main.js
+++ public/js/main.js
@@ -36,7 +36,7 @@ async function main(canvas) {
 	input.listenTo(window);
 
 	// debug collision layer
-	level.comp.layers.push(createCollisionLayer(level));
+	// level.comp.layers.push(createCollisionLayer(level));
 
 	// start game
 	const camera = new Camera();
